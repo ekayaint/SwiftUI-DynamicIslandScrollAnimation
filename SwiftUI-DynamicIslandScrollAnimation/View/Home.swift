b6ffb0e5bba06804363caa70ef3cb18257edc419
ekayaint <ekayaint@gmail.com>
2023-09-18 07:23:08 +0300

    Started

---
+++ SwiftUI-DynamicIslandScrollAnimation/View/Home.swift
@@ -0,0 +1,23 @@
+//
+//  Home.swift
+//  SwiftUI-DynamicIslandScrollAnimation
+//
+//  Created by ekayaint on 18.09.2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var size: CGSize
+    var safeArea: EdgeInsets
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
