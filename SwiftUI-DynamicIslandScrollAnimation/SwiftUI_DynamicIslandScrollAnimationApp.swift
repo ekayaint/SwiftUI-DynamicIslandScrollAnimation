4f0170eb9a682f009e6f4568faec0d38e30813fa
ekayaint <ekayaint@gmail.com>
2023-09-18 07:17:21 +0300

    Initial Commit

---
+++ SwiftUI-DynamicIslandScrollAnimation/SwiftUI_DynamicIslandScrollAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DynamicIslandScrollAnimationApp.swift
+//  SwiftUI-DynamicIslandScrollAnimation
+//
+//  Created by ekayaint on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DynamicIslandScrollAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
